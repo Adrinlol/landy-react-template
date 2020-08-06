60ac4c6be570b8474ed59f1ce07c2c9b4a7dc7ea
LashaKakabadze <lasha.kakabadze@falconix.com>
2020-08-06 11:08:42 +0400

    extensive styling cleanup

--- template/src/components/Block/styles.js
+++ template/src/components/Block/styles.js
@@ -1,35 +1,14 @@
 import styled from "styled-components";
 
 export const Content = styled.p`
-  font-size: 1rem;
-  font-family: "Source Sans Pro", sans-serif;
-  color: #0a1f44;
   margin-top: 1.5rem;
 `;
 
-export const Title = styled.h1`
-  font-size: 1.875rem;
-  font-weight: 700;
-  color: #0a1f44;
-  line-height: 2.0625rem;
-  max-width: 309px;
-  font-family: "Source Sans Pro", sans-serif;
-  @media only screen and (max-width: 414px) {
-    font-size: 1.625rem;
-    max-width: 340px;
-  }
-`;
+export const Title = styled.h1``;
 
 export const Container = styled.div`
   position: relative;
-  max-width: ${(props) => (props.padding ? "450px" : "700px")};
-  padding: ${(props) => (props.padding ? "0 0 6rem" : "")};
-  @media only screen and (max-width: 768px) {
-    padding: ${(props) => (props.padding ? "0 0 4rem" : "")};
-  }
-  @media only screen and (max-width: 414px) {
-    padding: ${(props) => (props.padding ? "0 0 0rem" : "")};
-  }
+  max-width: 700px;
 `;
 
 export const TextWrapper = styled.div`
