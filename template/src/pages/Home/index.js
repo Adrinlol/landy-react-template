f99fa956bab80ea7f292a82a9c6f46bf41cec256
LashaKakabadze <lasha.kakabadze@falconix.com>
2020-08-06 11:10:31 +0400

    updated router component

--- template/src/pages/Home/index.js
+++ template/src/pages/Home/index.js
@@ -1,96 +1,55 @@
-import React, { Fragment } from "react";
-import { Row, Col } from "antd";
+import React from "react";
 
 import ContactFrom from "../../components/ContactForm";
 import ContentBlock from "../../components/ContentBlock";
 import MiddleBlock from "../../components/MiddleBlock";
-
-import Button from "../../common/Button";
-import Input from "../../common/Input";
-import TextArea from "../../common/TextArea";
 import Container from "../../common/Container";
-import SvgIcon from "../../common/SvgIcon";
+
+import Introduction from "../../content/introduction.json";
+import FirstBlock from "../../content/firstBlock.json";
+import SecondBlock from "../../content/secondBlock.json";
+import ThirdBlock from "../../content/thirdBlock.json";
+import FourthBlock from "../../content/fourthBlock.json";
+import ContactBlock from "../../content/contactBlock.json";
 
 const Home = () => {
   return (
-    <Fragment>
+    <Container>
       <ContentBlock
         type="right"
-        title="✨Why even bother?"
-        content="This template is designed to quickly create react projects, with already developed responsive components and funcionalities, 
-        while still retaining all of the features of CRA."
-        button="Want to know more?"
-        icon="about-us.svg"
+        first="true"
+        title={Introduction.title}
+        content={Introduction.text}
+        button={Introduction.button}
+        icon="intro.svg"
       />
       <MiddleBlock
-        title="Benefits"
-        content="The amount of time I spend setting up new React projects has gone from few agonizing hours to a few blissful minutes. 
-        No more countless hours wasted on adding extra packages and boilerplates for routing."
-        button="Get started"
+        title={FirstBlock.title}
+        content={FirstBlock.text}
+        button={FirstBlock.button}
       />
       <ContentBlock
         type="left"
-        title="Left content block"
-        content="This template currently supports two types of blocks, left and right. The only difference between them being the positioning of text/image. 
-        Almost forgot, right block also has a button."
-        icon="team-goals.svg"
+        title={SecondBlock.title}
+        content={SecondBlock.text}
+        section={SecondBlock.section}
+        icon="mobile.svg"
       />
-      <MiddleBlock
-        title="Buttons"
-        content="Speaking of buttons, you can change the button color by passing - color property. By default it's blue, but it can also be:"
-        button="Blue"
+      <ContentBlock
+        type="right"
+        title={ThirdBlock.title}
+        content={FourthBlock.text}
+        icon="web.svg"
       />
-      <Container>
-        <Row type="flex" justify="space-between" align="middle">
-          <Button color="#E44945">Red</Button>
-          <Button color="#FEC03D">Yellow</Button>
-        </Row>
-      </Container>
-      <MiddleBlock
-        title="Input and textarea"
-        content="Next up we have a basic widget for text input and textarea. It also changes color on hover  :)"
+
+      <ContentBlock
+        type="left"
+        title={FourthBlock.title}
+        content={FourthBlock.text}
+        icon="mention.svg"
       />
-      <Container>
-        <Row type="flex" justify="center" align="middle">
-          <Col lg={6} md={12} sm={24} xs={24}>
-            <Input type="text" name="name" placeholder="Text Input" />
-            <TextArea placeholder="TextArea" />
-          </Col>
-        </Row>
-      </Container>
-      <MiddleBlock
-        title="Declarative Routing"
-        content="Routing is already set up and ready to go, to provide accessible navigation around your application. All you need to do is add new path and page name in config.js file."
-      />
-      <MiddleBlock
-        title="SVG Icons"
-        content="There are a lot of bits about working with React and SVG, and especially manipulating it, that aren’t quite supported yet. This component only receives src property, just pass the name of your SVG icon."
-      />
-      <Container>
-        <Row type="flex" justify="center" align="middle">
-          <Col lg={12} md={12} sm={24} xs={24}>
-            <SvgIcon
-              src="hexagonal-small.svg"
-              style={{ textAlign: "center" }}
-            />
-          </Col>
-        </Row>
-      </Container>
-      <MiddleBlock
-        title="SVG Decorations"
-        content="This is an experimental component that sets the provided SVG as a background.
-        It basically does the same thing as SVG Icon, but also supports absolute positioning, meaning that you can set it anywhere. "
-      />
-      <MiddleBlock
-        title="Header and Footer"
-        content="Last but not least, fully responsive footer & header, with both external and router links."
-      />
-      <MiddleBlock
-        title="Conclusion"
-        content="That’s all there is to it! Edit pages/Home/index.js and save to reload."
-      />
-      <ContactFrom />
-    </Fragment>
+      <ContactFrom title={ContactBlock.title} content={ContactBlock.text} />
+    </Container>
   );
 };
 
--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,43 +1,16 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
-import Loader from "react-loader-spinner";
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import routes from "./config";
-
-const TemporaryFallback = () => {
-  const style = {
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-  };
-  return (
-    <Loader
-      type="Triangle"
-      color="rgb(33, 36, 37)"
-      height={120}
-      width={120}
-      timeout={3000}
-      style={style}
-    />
-  );
-};
+import Home from "../pages/Home";
 
 const Router = () => {
   return (
-    <Suspense fallback={<TemporaryFallback />}>
+    <Suspense>
       <Header />
       <Switch>
-        {routes.map(({ path, page }, key) => (
-          <Route
-            key={key}
-            exact
-            path={`${path}`}
-            component={lazy(() => import(`../pages/${page}`))}
-          />
-        ))}
+        <Route exact path={"/"} component={Home} />
       </Switch>
       <Footer />
     </Suspense>
