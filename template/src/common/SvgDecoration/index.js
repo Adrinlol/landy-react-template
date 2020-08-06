8567fbe77ce24df4c94877bd6ff8a1803e7df3a3
LashaKakabadze <lasha.kakabadze@falconix.com>
2020-08-06 11:04:05 +0400

    deleted unused components

--- template/src/common/SvgDecoration/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import * as S from "./styles";
-
-const SvgDecoration = (props) => <S.Decoration {...props} />;
-
-export default SvgDecoration;
--- template/src/router/config.js
+++
@@ -1,3 +0,0 @@
-const routes = [{ path: "/", page: "Home" }];
-
-export default routes;
