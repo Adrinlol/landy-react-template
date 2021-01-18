0d8d6bf3d65e0351fd54ca8abf44a7e77e1455c5
LashaKakabadze <lasha.kakabadze@falconix.com>
2021-01-18 20:33:34 +0400

    removed antd scroll function and implemented my own

--- template/src/common/ScrollToTop/index.js
+++ template/src/common/ScrollToTop/index.js
@@ -1,15 +1,23 @@
-import React from "react";
-import { BackTop } from "antd";
-import SvgIcon from "../../common/SvgIcon";
+import React from 'react';
+import SvgIcon from '../../common/SvgIcon';
 
-import * as S from "./styles";
+import * as S from './styles';
 
-const Input = () => (
-  <BackTop>
-    <S.Up>
+const Input = () => {
+  const scrollUp = () => {
+    const element = document.getElementById('intro');
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+      inline: 'nearest',
+    });
+  };
+
+  return (
+    <S.Up onClick={scrollUp}>
       <SvgIcon src="scroll-top.svg" />
     </S.Up>
-  </BackTop>
-);
+  );
+};
 
 export default Input;
