2eab182a4a658a69157544a4e4005444eb47bea7
Lasha Kakabadze <l.qqbadze@gmail.com>
2021-04-10 12:26:20 +0400

    correct typo in svg component

--- template/src/common/SvgIcon/index.js
+++ template/src/common/SvgIcon/index.js
@@ -1,5 +1,5 @@
 const SvgIcon = ({ src, width, height }) => (
-  <img src={`/img/svg/${src}`} alt={src} with={width} height={height} />
+  <img src={`/img/svg/${src}`} alt={src} width={width} height={height} />
 );
 
 export default SvgIcon;
