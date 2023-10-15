b4ed93cebaa025c605fbbac23862a9d8628fbafe
Lasha Kakabadze <l.qqbadze@gmail.com>
2023-10-15 20:18:14 +0100

    migrate the project instead of the template #28

--- template/src/common/Container/index.tsx
+++
@@ -1,8 +0,0 @@
-import { StyledContainer } from "./styles";
-import { ContainerProps } from "../types";
-
-const Container = ({ border, children }: ContainerProps) => (
-  <StyledContainer border={border}>{children}</StyledContainer>
-);
-
-export default Container;
