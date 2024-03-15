b57f34b461c5641cdbdf415606768becb55ba4a1
paul-rayadev <paul@rayasolutionsph.com>
2024-03-15 10:27:12 +0800

    feat: routes for additional pages

--- src/router/config.ts
+++ src/router/config.ts
@@ -9,6 +9,21 @@ const routes = [
       path: "/support",
       exact: true,
       component: "Support"
+  },
+  {
+      path: "/contact",
+      exact: true,
+      component: "Contact"
+  },
+  {
+    path: "/pricing",
+    exact: true,
+    component: "Pricing"
+  },
+  {
+    path: "/services-products",
+    exact: true,
+    component: "ServicesProducts"
   }
 ];
 
