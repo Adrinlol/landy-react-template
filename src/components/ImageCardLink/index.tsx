3145f01d404b5e2114157910fbf024daf8a5ea98
paul-rayadev <paul@rayasolutionsph.com>
2024-03-18 18:38:44 +0800

    fix: revert image card link prop

--- src/components/ImageCardLink/index.tsx
+++ src/components/ImageCardLink/index.tsx
@@ -6,10 +6,11 @@ import { SvgIcon } from "../../common/SvgIcon";
 interface Props {
     image: string;
     link: string,
+    title: string;
     subtitle: string;
 }
 
-const ImageCardLink = ({ image, link, subtitle }: Props) => {
+const ImageCardLink = ({ image, link, title, subtitle }: Props) => {
     return (
         <a href={link} target="_blank">
             <Card>
