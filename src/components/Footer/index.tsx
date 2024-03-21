9b0e955f2821a85cbcca62ae4ccc52224b5f5d8b
paul-rayadev <paul@rayasolutionsph.com>
2024-03-21 21:34:50 +0800

    fix: remove template values footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -14,38 +14,38 @@ import {
 
 const Footer = ({ t }: { t: TFunction }) => {
   return (
-      <>
-        <FooterSection>
-          <Container>
-            <Row justify="space-between">
-              <Col lg={10} md={10} sm={12} xs={12}>
-                <Language>{t("Contact")}</Language>
-                <Large to="/support">{t("Tell us everything")}</Large>
-                <Large to="/support">{t("Customer Support")}</Large>
-                <Para>
-                  {t(`Do you have any question? Feel free to reach out.`)}
-                </Para>
-              </Col>
-              <Col lg={8} md={8} sm={12} xs={12}>
-                <Title>{t("Company")}</Title>
-                <Large to="/who-is-raya">{t("About")}</Large>
-              </Col>
-              <Col lg={6} md={6} sm={12} xs={12}>
-                <Empty />
-              </Col>
-            </Row>
-            <Row justify="space-between">
-              <Col lg={10} md={10} sm={12} xs={12}>
-                <Empty />
-                <Language>{t("Address")}</Language>
-                <Para>Cityland Mega Plaza</Para>
-                <Para>San Antonio, Pasig City</Para>
-                <Para>Metro Manila, Philippines</Para>
-              </Col>
-            </Row>
-          </Container>
-        </FooterSection>
-      </>
+    <>
+      <FooterSection>
+        <Container>
+          <Row justify="space-between">
+            <Col lg={10} md={10} sm={12} xs={12}>
+              <Language>{t("Contact")}</Language>
+              <Large to="/support">{t("Tell us everything")}</Large>
+              <Large to="/support">{t("Customer Support")}</Large>
+              <Para>
+                {t(`Do you have any question? Feel free to reach out.`)}
+              </Para>
+            </Col>
+            <Col lg={8} md={8} sm={12} xs={12}>
+              <Title>{t("Company")}</Title>
+              <Large to="/who-is-raya">{t("About")}</Large>
+            </Col>
+            <Col lg={6} md={6} sm={12} xs={12}>
+              <Empty />
+            </Col>
+          </Row>
+          <Row justify="space-between">
+            <Col lg={10} md={10} sm={12} xs={12}>
+              <Empty />
+              <Language>{t("Address")}</Language>
+              <Para>Cityland Mega Plaza</Para>
+              <Para>San Antonio, Pasig City</Para>
+              <Para>Metro Manila, Philippines</Para>
+            </Col>
+          </Row>
+        </Container>
+      </FooterSection>
+    </>
   );
 };
 
