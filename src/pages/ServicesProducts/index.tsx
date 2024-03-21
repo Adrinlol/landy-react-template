9fdda1c2e8c13c443c8facb0835226ee1af481fe
paul-rayadev <paul@rayasolutionsph.com>
2024-03-21 13:50:43 +0800

    feat: put contents from excel file

--- src/pages/ServicesProducts/index.tsx
+++ src/pages/ServicesProducts/index.tsx
@@ -6,18 +6,10 @@
 //  sections
 //
 import { lazy } from "react";
-import Overview from "../../content/ServicesProductsPage/OverviewContent.json";
 import ProductCategories from "../../content/ServicesProductsPage/ProductCategoriesContent.json";
-import IndividualProducts from "../../content/ServicesProductsPage/IndividualServicesProductsContent.json";
-import IndustrySolutions from "../../content/HomePage/IndustrySolutionsContent.json";
-import CallToAction from "../../content/ServicesProductsPage/CallToActionContent.json";
-import Testimonials from "../../content/ServicesProductsPage/TestimonialsContent.json";
-import Faq from "../../content/ServicesProductsPage/FaqContent.json";
 
 const MiddleBlock = lazy(() => import("../../components/MiddleBlock"));
 const Container = lazy(() => import("../../common/Container"));
-const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
-const ContentBlock = lazy(() => import("../../components/ContentBlock"));
 
 // TODO:
 //  follow raya color scheme from their logo
@@ -30,44 +22,10 @@ const ContentBlock = lazy(() => import("../../components/ContentBlock"));
 const ServicesProducts = () => {
     return (
         <Container>
-            <ScrollToTop />
             <MiddleBlock
-              title={Overview.title}
-              content={Overview.description}
-              button={Overview.button}/>
-            <ContentBlock
-                direction="right"
-                icon="product-launch.svg"
-                title={ProductCategories.title}
-                content={ProductCategories.description}
-                id="product-categories"/>
-            <ContentBlock
-                direction="left"
-                title={IndividualProducts.title}
-                content={IndividualProducts.text}
-                section={IndividualProducts.section}
-                icon=""
-                id="services-products"/>
-            <MiddleBlock
-                title={IndustrySolutions.title}
-                content={IndustrySolutions.description}
-                section={IndustrySolutions.solutions}/>
-            <MiddleBlock
-                title={CallToAction.title}
-                content={CallToAction.content}
-                button={CallToAction.button}
-                destination="contact"
-                destinationType="page"/>
-            <MiddleBlock
-                title={Testimonials.title}
-                content={Testimonials.description}
-                section={Testimonials.testimonials}/>
-            <ContentBlock
-                direction="right"
-                title={Faq.title}
-                content={Faq.description}
-                icon=""
-                id="faq"/>
+              title={ProductCategories.title}
+              content={ProductCategories.description}
+              section={ProductCategories.categories}/>
         </Container>
     );
 };
