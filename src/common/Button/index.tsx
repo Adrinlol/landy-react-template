57544ad19c7b415752c0c76da4c25066f1a2bd5a
paul-rayadev <paul@rayasolutionsph.com>
2024-03-21 20:32:56 +0800

    fix(refactor): conditional rendering button wrapper

--- src/common/Button/index.tsx
+++ src/common/Button/index.tsx
@@ -1,8 +1,32 @@
 import { StyledButton } from "./styles";
 import { ButtonProps } from "../types";
+import {ButtonWrapper} from "../../components/ContentBlock/styles";
 
 export const Button = ({ color, children, onClick }: ButtonProps) => (
   <StyledButton color={color} onClick={onClick}>
     {children}
   </StyledButton>
-);
+)
+export const ConditionalButtonWrapper = (
+    item: {
+      color?: string;
+      title: string;
+      buttonCount: number;
+    },
+    id: number) => {
+  return (
+      item.buttonCount > 1 ?
+          <ButtonWrapper>
+            <Button
+                key={id}
+                color={item.color}>
+              {item.title}
+            </Button>
+          </ButtonWrapper> :
+          <Button
+              key={id}
+              color={item.color}>
+            {item.title}
+          </Button>
+  )
+}
