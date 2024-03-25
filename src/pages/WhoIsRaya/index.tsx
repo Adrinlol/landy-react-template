9da7b82301c8ade67ba23436548dfd7a66d7ba7a
paul-rayadev <paul@rayasolutionsph.com>
2024-03-26 01:00:02 +0800

    fix: unused imports

--- src/pages/WhoIsRaya/index.tsx
+++ src/pages/WhoIsRaya/index.tsx
@@ -1,29 +1,15 @@
 import "./styles.css"
 import React, {lazy} from "react";
 import WhoIsRayaContent from "../../content/WhoIsRayaPage/WhoIsRayaConent.json"
-import ContentBlock from "../../components/ContentBlock";
-import {MinSubtitle, MinTitle} from "../TailoredSolutions/styles";
-import {Button, Carousel} from "antd";
-import {LeftCircleFilled, LeftCircleTwoTone, RightCircleFilled, RightCircleTwoTone} from "@ant-design/icons";
+import {MinSubtitle} from "../TailoredSolutions/styles";
+import {Carousel} from "antd";
+import {LeftCircleFilled, RightCircleFilled} from "@ant-design/icons";
 import {MinPara} from "../../components/ContentBlock/styles";
 
 
 const Container = lazy(() => import("../../common/Container"));
-const styleDefaults = {
-    height: 300,
-    color: "white",
-    fontSize: 100,
-    textAlign: "center"
-};
 
 const WhoIsRaya = () => {
-    let settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    };
     return (
         <Container>
             <Carousel
