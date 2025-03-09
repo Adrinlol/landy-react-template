d5829d5e9c1498d15395a7ed72837c499581800c
Lasha <l.qqbadze@gmail.com>
2025-03-09 09:51:02 +0400

    Fix: Ignore Ant Design source map warnings

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import { I18nextProvider } from "react-i18next";
-import "antd/dist/antd.css";
+import 'antd/dist/antd.min.css';
 
 import Router from "./router";
 import i18n from "./translation";
